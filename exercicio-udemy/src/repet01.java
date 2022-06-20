59ace31c03d59a9e43a7af5090efada4cf80438a
Pedro Padilha Neto <pedropadilhan@gmail.com>
2022-06-20 17:49:56 -0300

    Exercicios WHILE

---
+++ exercicio-udemy/src/repet01.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class repet01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int senha = sc.nextInt();
+		
+		while(senha != 2002) {
+			System.out.println("Senha invalida");
+			senha = sc.nextInt();
+		}
+		
+		System.out.println("Acesso permitido");
+		
+		sc.close();
+	}
+
+}
